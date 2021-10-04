f97098198de26ae63d49cdae37ac8fe3b7b10ceb
Bhautik Makwana <bhautik.makwana@civica2.com>
2021-10-04 13:56:13 +0530

    GK Notes

--- sublime/code.cpp
+++ sublime/code.cpp
@@ -1,8 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+
 int main() {
 
-	
+	// int arr[2] = {1, 2};
+	int arr[2][3] = {{1, 2, 3}, {4, 5, 6}};
+
+	int i = j = 0;
+
+	for (i; i < 2; i++) {
+		for(j; j < 3; j++) {
+			cout << arr[i][j] << ' ';
+		}
+		cout << endl;
+	}
+
+
+
 
 }
